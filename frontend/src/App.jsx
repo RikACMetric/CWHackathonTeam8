dcca3acfb015a2691a26fea283dd25ff250826da
Ruben Nascimento <ruben@cleverfranke.com>
2026-04-09 13:23:50 +0200

    Add sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,8 +9,10 @@ export default function App() {
 
   return (
     <>
-      <Header />      
+      <Header />  
+
       <div className="main">        
+        <Sidebar  firePrompt={firePrompt} />
         <ChatArea
           messages={messages}
           typing={typing}
